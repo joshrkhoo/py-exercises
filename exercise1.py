d7d22d7e9d31d661fb5812ab4b97b2550a81473f
Triceratops <75021575+joshrkhoo@users.noreply.github.com>
2022-12-31 15:56:47 +1100

    Finished exercise 1

---
+++ exercise1.py
@@ -0,0 +1,39 @@
+"""
+1. Create a program that asks the user to enter their name and their age. 
+
+2. Print out a message addressed to them that tells them the year that    
+    they will turn 100 years old.
+"""
+import datetime
+
+name = input("What is your name?")
+age = int(input("What is your age?"))
+
+print("I am " + name  + " and i am "  + str(age) + " years old")
+
+years = 100 - age
+
+print ("I will turn 100 in " + str(years) +" years")
+
+today = datetime.date.today()
+year = today.year 
+print (year)
+
+print ("You will be 100 years old in " + str(years + year))
+
+"""
+1. 100 minus age = number of years till they turn 100
+2. print variable of number of years
+3. get current date
+4. add number of years to current date 
+5. get that date
+"""
+
+"""
+What I learnt
+    - input() function
+    - manipulating strings using the function 'int()'
+    - using the function str() 
+    - concatenate: combine
+    - how to get current date
+""" 
