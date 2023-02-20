57e1094e005141c1e46f6a59f633b1a65719c98b
Triceratops <75021575+joshrkhoo@users.noreply.github.com>
2023-02-20 18:33:10 +1100

    starting exercise6.py

---
+++ exercise6.py
@@ -0,0 +1,34 @@
+"""
+Ask the user for a string and print out whether this string is a palindrome or not. 
+(A palindrome is a string that reads the same forwards and backwards.)
+"""
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(a[1:5:4])
+#This prints [2]
+
+x = input("type a string")
+b = []
+x.append(b)
+
+print(b)
+
+
+
+"""
+NOTES
+
+1. list indexing
+- start counting lists from number 0
+- use var[] to get the elem of the list
+- or use[:] to get a range not including the last elem chosen
+    - a[1,2,3,4,5]
+    - a[0:3] would give [1,2,3] (easy way to get sublists between two indices)
+    - a third number in indexing counts how often you should read from the list
+        - [1:5:4] would mean:
+            give numbers in list between num1 and num5, starting at num1 and goin up by 4s
+
+
+
+"""
+
