3f188f8d00367daa349e61d6891c035af19b6762
joshrkhoo <johndoe@email.com>
2023-02-21 19:08:03 +1100

    starting exercise7.py

---
+++ exercise7.py
@@ -0,0 +1,15 @@
+"""
+Let’s say I give you a list saved in a variable:
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. 
+Write one line of Python that takes this list a and makes a new list that has only the 
+even elements of this list in it.
+"""
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+even_nums = []
+
+for elem in a:
+    if elem % 2 == 0:
+         even_nums.append(elem)
+    else:pass
+print(even_nums)
